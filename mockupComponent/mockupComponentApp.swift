957763ec70a34ec8cc7855ccc0e4ec1841e8b2dd
SeoChangHo <80376144+Seo-Changho@users.noreply.github.com>
2024-02-21 15:56:29 +0900

    Add files via upload

---
+++ mockupComponent/mockupComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  mockupComponentApp.swift
+//  mockupComponent
+//
+//  Created by changho seo on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct mockupComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
