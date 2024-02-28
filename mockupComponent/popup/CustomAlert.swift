17d1b70cf2b5f1a8679045b29d645cf1c44f68a0
changho <chseo@hyundaiht.co.kr>
2024-02-28 13:44:11 +0900

    popup view에 여러 contents 추가 후 테스트진행

--- mockupComponent/popup/CustomAlert.swift
+++ mockupComponent/popup/CustomAlert.swift
@@ -25,6 +25,10 @@ enum contentType {
     case title
     case message
     case checkbox
+    case textfield
+    case imageView
+    case pickerView
+    case radioView
 }
 
 /// popup에 들어갈 button case를 지정하여 확장한다.
@@ -32,6 +36,7 @@ enum buttonType {
     case okbutton
     case cancelbutton
     case checkbutton
+    case stringbutton
 }
 
 
@@ -48,8 +53,8 @@ enum buttonType {
  
  ```swift
  VStack{
-    alertOption
-    buttonOption
+ alertOption
+ buttonOption
  }
  ```
  ![customAlert basic](customalert_basic.png)
@@ -61,11 +66,20 @@ struct CustomAlert: View {
     @State var alertOptions: [alertOption] = []
     @State var buttonOptions : [buttonOption] = []
     @State private var isOn = false
+    @State private var textString : String = ""
+    
+    @State private var count = 3
+    @State private var isCounting = false
+    @State private var timer: Timer?
+    
+    @State private var selectedOption: Int = 0
     
     var isShowVerticalButtons = false
     
     var cancelButtonAction: (() -> ())?
     var okButtonAction: (() -> ())?
+    var checkButtonAction: (() -> ())?
+    var stringButtonAction: ((String) -> ())?
     
     let verticalButtonsHeight: CGFloat = 80
     
@@ -79,7 +93,7 @@ struct CustomAlert: View {
                 HStack{
                     Spacer(minLength: 16)
                     VStack(spacing: 0) {
-                        ForEach(Array(alertOptions.enumerated()), id: \.offset){ index, option in
+                        ForEach(alertOptions, id: \.id){ option in
                             switch option.contentType {
                             case .title:
                                 let value = option.option as? String ?? ""
@@ -105,17 +119,80 @@ struct CustomAlert: View {
                                     HStack(alignment: .top, spacing: 10) {
                                         Image(self.isOn ? "checkbox_button_s" : "checkbox_button_n")
                                         Text(value)
+                                            .frame(minHeight: 0, maxHeight: .infinity)
+                                            .font(.system(size: 14))
+                                            .foregroundColor(Color(red: 52/255,green: 57/255,blue: 68/255))
+                                            .lineLimit(nil)
                                     }
                                 }
+                                .padding([.top], 16)
                                 .foregroundColor(Color(red: 52/255,green: 57/255,blue: 68/255))
                                 .fixedSize(horizontal: false, vertical: true)
+                                .buttonStyle(PlainButtonStyle())
+                            case .textfield:
+                                let value = option.option as? String ?? ""
+                                TextField(value, text: $textString)
+                                    .padding([.top, .bottom], 10)
+                                    .background(Color.white)
+                                    .onChange(of: textString) { _ in
+                                        self.isOn = textString != ""
+                                    }
+                                
+                                Rectangle()
+                                    .frame(height: 2)
+                                    .foregroundColor(self.isOn ? Color(red: 52/255,green: 57/255,blue: 68/255) : .gray.opacity(50))
+                            case .imageView:
+                                let value = option.option as? String ?? ""
+                                Image(value)
+                                Text("\(count)")
+                                    .padding([.bottom], 10)
+                                    .onAppear(){
+                                        startTimer()
+                                    }
+                            case .pickerView:
+                                let value = option.option as? [String] ?? []
+                                Picker("picker view", selection: $textString) {
+                                    ForEach(value, id: \.self){ item in
+                                        Text(item)
+                                    }
+                                }
+                                .pickerStyle(WheelPickerStyle())
+                                .onChange(of: textString) { item in
+                                    self.isOn = true
+                                    textString = item
+                                }
+                            case .radioView:
+                                let value = option.option as? [String] ?? []
+                                ForEach(value.indices, id: \.self){ index in
+                                    VStack {
+                                        Button(action: {
+                                            self.isOn = true
+                                            self.selectedOption = index
+                                            self.textString = value[index]
+                                        }) {
+                                            HStack(alignment: .top, spacing: 10) {
+                                                Text(value[index])
+                                                    .frame(minHeight: 0, maxHeight: .infinity)
+                                                    .font(.system(size: 14))
+                                                    .foregroundColor(Color(red: 52/255,green: 57/255,blue: 68/255))
+                                                    .lineLimit(nil)
+                                                Spacer()
+                                                Image(self.selectedOption == index ? "radio_button_s" : "radio_button_n")
+                                            }
+                                        }
+                                        .padding([.top], 16)
+                                        .foregroundColor(Color(red: 52/255,green: 57/255,blue: 68/255))
+                                        .fixedSize(horizontal: false, vertical: true)
+                                        .buttonStyle(PlainButtonStyle())
+                                    }
+                                }
                             }
                         }
                         
                         
                         /// 하단 버튼 영역
                         HStack(spacing: 0) {
-                            ForEach(Array(buttonOptions.enumerated()), id: \.offset) { index, button in
+                            ForEach(buttonOptions, id: \.id) { button in
                                 switch button.buttonType {
                                 case .cancelbutton:
                                     Button {
@@ -148,17 +225,31 @@ struct CustomAlert: View {
                                     }
                                 case .checkbutton:
                                     Button {
-                                        okButtonAction?()
+                                        if isOn {checkButtonAction?()}
                                     } label: {
                                         Text("확인")
                                             .padding()
                                             .frame(minWidth: 0, maxWidth: geometry.size.width*0.5, minHeight: 0, maxHeight: .infinity)
-                                            .background(Color(red: 52/255,green: 57/255,blue: 68/255))
+                                            .background(isOn ? Color(red: 52/255,green: 57/255,blue: 68/255) : .gray)
                                             .font(.system(size: 12, weight: .bold))
                                             .foregroundColor(.white)
                                             .cornerRadius(25)
                                             .multilineTextAlignment(.center)
-                                            .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color(red: 52/255,green: 57/255,blue: 68/255), lineWidth: 1))
+                                            .overlay(RoundedRectangle(cornerRadius: 25).stroke(isOn ? Color(red: 52/255,green: 57/255,blue: 68/255) : .gray, lineWidth: 1))
+                                    }
+                                case .stringbutton:
+                                    Button {
+                                        if isOn {stringButtonAction?(textString)}
+                                    } label: {
+                                        Text("확인")
+                                            .padding()
+                                            .frame(minWidth: 0, maxWidth: geometry.size.width*0.5, minHeight: 0, maxHeight: .infinity)
+                                            .background(isOn ? Color(red: 52/255,green: 57/255,blue: 68/255) : .gray)
+                                            .font(.system(size: 12, weight: .bold))
+                                            .foregroundColor(.white)
+                                            .cornerRadius(25)
+                                            .multilineTextAlignment(.center)
+                                            .overlay(RoundedRectangle(cornerRadius: 25).stroke(isOn ? Color(red: 52/255,green: 57/255,blue: 68/255) : .gray, lineWidth: 1))
                                     }
                                 }
                             }
@@ -175,6 +266,21 @@ struct CustomAlert: View {
                 }
             }
             .zIndex(2)
+        }
+    }
+    
+    func startTimer(){
+        count = 3
+        isCounting = true
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            count -= 1
+            
+            if count <= 0 {
+                timer.invalidate()
+                isCounting = false
+                presentAlert = false
+            }
         }
     }
 }
--- mockupComponent/popup/popupView.swift
+++ mockupComponent/popup/popupView.swift
@@ -11,10 +11,19 @@ struct popupView: View {
     @State private var okbuttonAlert = false
     @State private var cancelbuttonAlert = false
     @State private var toggleAlert = false
+    @State private var textAlert = false
+    @State private var imageAlert = false
+    @State private var pickerAlert = false
+    @State private var radioAlert = false
+    
+    @State private var resultString = ""
     
     var body: some View {
         ZStack {
             VStack{
+                
+                Text(resultString)
+
                 Button(action: {
                     self.okbuttonAlert.toggle()
                 }) {
@@ -24,7 +33,7 @@ struct popupView: View {
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 }
-                
+                 
                 Button(action: {
                     self.cancelbuttonAlert.toggle()
                 }) {
@@ -34,11 +43,51 @@ struct popupView: View {
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 }
-                
+
                 Button(action: {
                     self.toggleAlert.toggle()
                 }) {
-                    Text("twoButton")
+                    Text("checkButton")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    self.textAlert.toggle()
+                }) {
+                    Text("textFieldButton")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    self.imageAlert.toggle()
+                }) {
+                    Text("imageViewButton")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    self.pickerAlert.toggle()
+                }) {
+                    Text("pickerButton")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    self.radioAlert.toggle()
+                }) {
+                    Text("radioButton")
                         .padding()
                         .background(Color.blue)
                         .foregroundColor(.white)
@@ -49,6 +98,7 @@ struct popupView: View {
             if okbuttonAlert {
                 CustomAlert(presentAlert: $okbuttonAlert, alertOptions: [alertOption(option: "메시지 입니다.", contentType: .message)], buttonOptions: [buttonOption(buttonType: .okbutton)], okButtonAction: {
                     okbuttonAlert.toggle()
+                    self.resultString = "success"
                 })
             }
             
@@ -57,14 +107,47 @@ struct popupView: View {
                     cancelbuttonAlert.toggle()
                 }, okButtonAction: {
                     cancelbuttonAlert.toggle()
+                    self.resultString = "success2"
                 })
             }
             
             if toggleAlert {
-                CustomAlert(presentAlert: $toggleAlert, alertOptions: [alertOption(option: "토글버튼 입니다.", contentType: .checkbox)], buttonOptions: [buttonOption(buttonType: .cancelbutton), buttonOption(buttonType: .checkbutton)], cancelButtonAction: {
+                CustomAlert(presentAlert: $toggleAlert, alertOptions: [alertOption(option: "토글버튼을 선택할 건지에 대한 질문 입니다.", contentType: .message), alertOption(option: "토글버튼 입니다.", contentType: .checkbox)], buttonOptions: [buttonOption(buttonType: .cancelbutton), buttonOption(buttonType: .checkbutton)], cancelButtonAction: {
                     toggleAlert.toggle()
-                }, okButtonAction: {
+                }, checkButtonAction: {
                     toggleAlert.toggle()
+                    self.resultString = "success3"
+                })
+            }
+            
+            if textAlert {
+                CustomAlert(presentAlert: $textAlert, alertOptions: [alertOption(option: "placeholder", contentType: .textfield)], buttonOptions: [buttonOption(buttonType: .cancelbutton), buttonOption(buttonType: .stringbutton)], cancelButtonAction: {
+                    textAlert.toggle()
+                }, stringButtonAction: { result in
+                    textAlert.toggle()
+                    self.resultString = result
+                })
+            }
+            
+            if imageAlert {
+                CustomAlert(presentAlert: $imageAlert, alertOptions: [alertOption(option: "popup_register_processing", contentType: .imageView) ,alertOption(option: "기기 및 서비스와 연결중이며\n1분 이상 걸릴 수 있습니다.", contentType: .message)], buttonOptions: [])
+            }
+            
+            if pickerAlert {
+                CustomAlert(presentAlert: $textAlert, alertOptions: [alertOption(option: ["apple", "banana", "cherry", "orange"], contentType: .pickerView)], buttonOptions: [buttonOption(buttonType: .cancelbutton), buttonOption(buttonType: .stringbutton)], cancelButtonAction: {
+                    pickerAlert.toggle()
+                }, stringButtonAction: { result in
+                    pickerAlert.toggle()
+                    self.resultString = result
+                })
+            }
+            
+            if radioAlert {
+                CustomAlert(presentAlert: $radioAlert, alertOptions: [alertOption(option: "라디오 버튼 리스트 컨트롤", contentType: .message), alertOption(option: ["1", "2", "3"], contentType: .radioView)], buttonOptions: [buttonOption(buttonType: .cancelbutton), buttonOption(buttonType: .stringbutton)], cancelButtonAction: {
+                    radioAlert.toggle()
+                }, stringButtonAction: { result in
+                    radioAlert.toggle()
+                    self.resultString = result
                 })
             }
         }
